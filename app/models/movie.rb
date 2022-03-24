cd77df8a3a3dae46dd34dad8973d610d6cfd7edf
SABAH EL MOUHSINI <elmosabah@gmail.com>
2022-03-24 15:11:41 +0100

    models, migrations, validations, relations

---
+++ app/models/movie.rb
@@ -0,0 +1,11 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+  # belongs_to :movie
+  # belongs_to :list
+  # validates overview?
+  # validates :title, uniqueness: true, presence: true
+  # validates :overview, presence: true
+end
