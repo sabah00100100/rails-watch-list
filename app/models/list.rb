333e4b6ceee566f303a7f93729ceace4e073d8b7
SABAH EL MOUHSINI <elmosabah@gmail.com>
2022-08-26 10:42:35 +0100

    some front and seed check

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,15 +1,15 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
-  has_many :movies, through: :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
   validates :name, uniqueness: true, presence: true
   # has_many :bookmarks, dependent: :destroy
   # has_many :movies, through: :bookmarks
 
-  include PgSearch::Model
-  pg_search_scope :search_by_name,
-  against: [ :name ],
-  using: {
-    tsearch: { prefix: true }
+  # include PgSearch::Model
+  # pg_search_scope :search_by_name,
+  # against: [ :name ],
+  # using: {
+  #   tsearch: { prefix: true }
     # <-- now `superman batm` will return something!
-  }
+
 end
